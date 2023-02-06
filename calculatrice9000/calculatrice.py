31ef50ee659a3d541855cb5d8a3b24a492ea4f61
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-02-06 16:28:44 +0100

    Calcul au carré + couleurs

--- calculatrice9000/calculatrice.py
+++ calculatrice9000/calculatrice.py
@@ -24,6 +24,12 @@ def square_root_func():
     nb_entry.delete(0, tk.END)
     nb_entry.insert(0, squareRoot)
 
+# Calcul carré
+def square_func():
+    square = float(nb_entry.get())*float(nb_entry.get())
+    nb_entry.delete(0, tk.END)
+    nb_entry.insert(0, square)
+
 # Résultat
 def result():
     try:
@@ -34,28 +40,29 @@ def result():
         messagebox.showinfo("Erreur", "Erreur de syntaxe.")
 
 # Nombres
-b1 = Button(root, text=1, padx=40, pady=20, command=lambda: click_btn(1))
-b2 = Button(root, text=2, padx=40, pady=20, command=lambda: click_btn(2))
-b3 = Button(root, text=3, padx=40, pady=20, command=lambda: click_btn(3))
-b4 = Button(root, text=4, padx=40, pady=20, command=lambda: click_btn(4))
-b5 = Button(root, text=5, padx=40, pady=20, command=lambda: click_btn(5))
-b6 = Button(root, text=6, padx=40, pady=20, command=lambda: click_btn(6))
-b7 = Button(root, text=7, padx=40, pady=20, command=lambda: click_btn(7))
-b8 = Button(root, text=8, padx=40, pady=20, command=lambda: click_btn(8))
-b9 = Button(root, text=9, padx=40, pady=20, command=lambda: click_btn(9))
-b0 = Button(root, text=0, padx=40, pady=20, command=lambda: click_btn(0))
+b1 = Button(root, text=1, padx=40, pady=20, bg="#8E8EE5", activebackground='#7A7AC6', command=lambda: click_btn(1))
+b2 = Button(root, text=2, padx=40, pady=20, bg="#8E8EE5", activebackground='#7A7AC6', command=lambda: click_btn(2))
+b3 = Button(root, text=3, padx=40, pady=20, bg="#8E8EE5", activebackground='#7A7AC6', command=lambda: click_btn(3))
+b4 = Button(root, text=4, padx=40, pady=20, bg="#7A7AC5", activebackground='#7373BB', command=lambda: click_btn(4))
+b5 = Button(root, text=5, padx=40, pady=20, bg="#7A7AC5", activebackground='#7373BB', command=lambda: click_btn(5))
+b6 = Button(root, text=6, padx=40, pady=20, bg="#7A7AC5", activebackground='#7373BB', command=lambda: click_btn(6))
+b7 = Button(root, text=7, padx=40, pady=20, bg="#6666CD", activebackground='#6161C2', command=lambda: click_btn(7))
+b8 = Button(root, text=8, padx=40, pady=20, bg="#6666CD", activebackground='#6161C2', command=lambda: click_btn(8))
+b9 = Button(root, text=9, padx=40, pady=20, bg="#6666CD", activebackground='#6161C2', command=lambda: click_btn(9))
+b0 = Button(root, text=0, padx=40, pady=20, bg="#9898F5", activebackground='#FFFF72', command=lambda: click_btn(0))
 
 # Opérateurs
-add = Button(root, text='+', padx=40, pady=20, command=lambda: click_btn('+'))
-subtract = Button(root, text='-', padx=40, pady=20, command=lambda: click_btn('-'))
-multiply = Button(root, text='*', padx=40, pady=20, command=lambda: click_btn('*'))
-divide = Button(root, text='/', padx=40, pady=20, command=lambda: click_btn('/'))
-comma = Button(root, text='.', padx=40, pady=20, command=lambda: click_btn('.'))
-square_root = Button(root, text='√x', padx=40, pady=20, command=square_root_func)
-equal = Button(root, text='=', padx=40, pady=20, command=result)
+add = Button(root, text='+', padx=40, pady=20, bg="#AFE961", activebackground='#A5DD5B', command=lambda: click_btn('+'))
+subtract = Button(root, text='-', padx=40, pady=20, bg="#AFE961", activebackground='#A5DD5B', command=lambda: click_btn('-'))
+multiply = Button(root, text='*', padx=40, pady=20, bg="#AFE961", activebackground='#A5DD5B', command=lambda: click_btn('*'))
+divide = Button(root, text='/', padx=40, pady=20, bg="#AFE961", activebackground='#A5DD5B', command=lambda: click_btn('/'))
+comma = Button(root, text='.', padx=40, pady=20, bg="#AFE961", activebackground='#A5DD5B', command=lambda: click_btn('.'))
+square_root = Button(root, text='√x', padx=40, pady=20, bg="#AFE961", activebackground='#A5DD5B', command=square_root_func)
+square = Button(root, text='x²', padx=40, pady=20, bg="#AFE961", activebackground='#A5DD5B', command=square_func)
+equal = Button(root, text='=', padx=40, pady=20, bg="#FF3636", activebackground='#E83131', command=result)
 
 # Effacer
-clear = Button(root, text='Clear', padx=30, pady=20, command=clear_entry)
+clear = Button(root, text='Clear', padx=30, pady=20, bg="#FF3636", activebackground='#E83131', command=clear_entry)
 
 # Afficher dans l'interface graphique
 b1.grid(row=3, column=0)
@@ -67,16 +74,17 @@ b6.grid(row=2, column=2)
 b7.grid(row=1, column=0)
 b8.grid(row=1, column=1)
 b9.grid(row=1, column=2)
-b0.grid(row=4, column=0)
+b0.grid(row=4, column=1)
 
 add.grid(row=1,column=3)
 subtract.grid(row=2,column=3)
 multiply.grid(row=3,column=3)
 divide.grid(row=4,column=3)
-comma.grid(row=4,column=1)
-equal.grid(row=4,column=2)
+comma.grid(row=4,column=0)
+equal.grid(row=5,column=3)
 
 clear.grid(row=5,column=0)
-square_root.grid(row=5,column=1)
+square_root.grid(row=4,column=2)
+square.grid(row=5,column=1)
 
 root.mainloop()
